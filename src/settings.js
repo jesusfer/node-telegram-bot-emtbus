809411d95dc5f6a370dd6b8c39f8d5ae573fceda
Jesús Fernández <jesus@nublar.net>
2016-10-23 16:35:28 +0200

    Render the arriving list with a monospace font and in a table-like fashion so it's easier to read

--- src/settings.js
+++ src/settings.js
@@ -12,7 +12,10 @@ const settings = {
     emt_linesxml: './data/Lines.xml',
     emt_nodesxml: './data/NodesLines.xml',
 
-    result_thumb: 'http://i.imgur.com/IG5PB4z.png'
+    result_thumb: 'http://i.imgur.com/IG5PB4z.png',
+	// Max column width for the results table
+	// For small screen phones it's still too much
+    maxColumnWidth: 18
 };
 
 module.exports = settings;
